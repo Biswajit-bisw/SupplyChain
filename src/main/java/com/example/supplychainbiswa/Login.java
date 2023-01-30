e6c1ae25a37f81e6a30ff682fa0a21d9f2e99bd8
Biswajit-bisw <biswajitbiswal2803@gmail.com>
2023-01-31 01:58:29 +0530

    Login Encryption

--- src/main/java/com/example/supplychainbiswa/Login.java
+++ src/main/java/com/example/supplychainbiswa/Login.java
@@ -1,8 +1,32 @@
 package com.example.supplychainbiswa;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.sql.ResultSet;
 
 public class Login {
+    private byte[] getSHA(String input){
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            return messageDigest.digest(input.getBytes(StandardCharsets.UTF_8));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private String getEncryptedPassword(String password){
+        String encryptedPassword= "";
+        try{
+            BigInteger number=new BigInteger(1,getSHA(password));
+            StringBuilder hexString =new StringBuilder(number.toString(16));
+            return hexString.toString();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
     public boolean customerLogin(String email,String password) {
         String query = String.format("SELECT * FROM customer WHERE email ='%s' AND password = '%s'", email, password);
 
@@ -18,8 +42,12 @@ public class Login {
 
         return false;
     }
-    public static void main(String[] args){
-        Login login =new Login();
-        System.out.println(login.customerLogin( "biswajitbiswal2002@gmail.com", "abc123"));
-    }
+//    public static void main(String[] args){
+//        Login login =new Login();
+//        System.out.println(login.customerLogin( "biswajitbiswal2002@gmail.com", "abc123"));
+//    }
+//    public static void main(String[] args){
+//        Login login=new Login();
+//        System.out.println(login.getEncryptedPassword("abcgjggg dddddddddddddddddddd dddddddddddd ddddddddddddddddd ddddddddd 123"));
+//    }
 }
---
+++ src/main/java/com/example/supplychainbiswa/ProductDetails.java
@@ -0,0 +1,2 @@
+package com.example.supplychainbiswa;public class ProductDetails {
+}
